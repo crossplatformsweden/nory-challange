bd33d9c2c7cffcce6aa3bdf7ca38b6c337dee374
Emil Apelgren <emilapelgren@outlook.com>
2025-05-18 11:57:11 +0100

    fix: add missing dependencies and types for eslint-config

---
+++ packages/eslint-config/types.d.ts
@@ -0,0 +1,7 @@
+declare module 'globals';
+declare module 'eslint-config-prettier';
+declare module 'eslint-plugin-only-warn';
+declare module 'eslint-plugin-turbo';
+declare module '@next/eslint-plugin-next';
+declare module 'eslint-plugin-react';
+declare module 'eslint-plugin-react-hooks';
